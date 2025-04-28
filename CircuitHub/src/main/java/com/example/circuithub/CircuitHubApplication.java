59df02f17b52839e9e3b6632d859f8d9e2c55598
Karu <169058702+KArruuuu@users.noreply.github.com>
2025-04-28 19:16:16 +0800

    added springboot framework

---
+++ CircuitHub/src/main/java/com/example/circuithub/CircuitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.circuithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircuitHubApplication.class, args);
+    }
+
+}
